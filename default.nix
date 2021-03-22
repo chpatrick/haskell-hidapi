5e9de3d7b5b1c03770da158d0bfc8954644bb6e1
Patrick Chilton <chpatrick@gmail.com>
2021-03-22 21:11:49 +0100

    Version 0.1.6

--- default.nix
+++ default.nix
@@ -4,9 +4,9 @@
 let
   baseDrv = pkgs.haskellPackages.callCabal2nix "hidapi" ./. {};
   drv = pkgs.haskell.lib.overrideCabal baseDrv (old: {
-    librarySystemDepends = old.librarySystemDepends ++ [ pkgs.udev pkgs.udev.lib ];
+    librarySystemDepends = old.librarySystemDepends ++ [ pkgs.udev pkgs.udev ];
     shellHook = ''
-      export LD_LIBRARY_PATH=${pkgs.udev.lib}/lib:$LD_LIBRARY_PATH
+      export LD_LIBRARY_PATH=${pkgs.udev}/lib:$LD_LIBRARY_PATH
     '';
   });
 in if pkgs.lib.inNixShell then drv.env else drv
